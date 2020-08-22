b0e3e7c7fd4eb6c22b7d66d12f670641758e48cb
arjunmalhotra1 <am2cj@virginia.edu>
2020-08-22 16:50:51 -0500

    Tried addign a few functions

--- 001-Hello-World/hello.go
+++ 001-Hello-World/hello.go
@@ -6,9 +6,22 @@ func main() {
 
 	fmt.Println("Hello Everyone!")
 	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
 }
 
 func foo() {
 
 	fmt.Println("I am in foo")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
 }
